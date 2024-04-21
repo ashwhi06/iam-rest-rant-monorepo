2b84b563a66045e30f86b3eeca15fc9a69873715
ashwhi06 <ashe.white06@gmail.com>
2024-04-21 14:26:40 -0400

    Fixing CORS error

--- frontend/src/places/EditPlaceForm.js
+++ frontend/src/places/EditPlaceForm.js
@@ -30,7 +30,8 @@ function EditPlaceForm() {
 		await fetch(`http://localhost:5000/places/${place.placeId}`, {
 			method: 'PUT',
 			headers: {
-				'Content-Type': 'application/json'
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin' : '*'
 			},
 			body: JSON.stringify(place)
 		})
