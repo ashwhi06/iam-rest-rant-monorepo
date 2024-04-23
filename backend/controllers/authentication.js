cffba3d52bc270603b73c25a1ec97c570c9c85a2
ashwhi06 <ashe.white06@gmail.com>
2024-04-23 19:48:03 -0400

    fetching login data

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,11 +1,21 @@
-const router = require('express').Router()
-const db = require("../models")
-const bcrypt = require('bcrypt')
+const router = require("express").Router();
+const db = require("../models");
+const bcrypt = require("bcrypt");
 
-const { User } = db
+const { User } = db;
 
-router.post('/', async (req, res) => {
-    console.log('IN HERE')
-})
+router.post("/", async (req, res) => {
+  let user = await User.findOne({
+    where: {email: req.body.email}
 
-module.exports = router
+  });
+  if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
+    res.status(401).json({
+      error: "Could not authenticate user"
+    })
+  } else {
+    res.json({user});
+  }
+});
+
+module.exports = router;
